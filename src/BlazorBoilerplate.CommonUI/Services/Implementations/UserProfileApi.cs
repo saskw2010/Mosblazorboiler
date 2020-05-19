23a3511d164f2358c2ee6f64a431ccf8990222c0
saskw2010 <saskw2010@gmail.com>
2020-05-19 09:58:04 +0300

    Initial commit

---
+++ src/BlazorBoilerplate.CommonUI/Services/Implementations/UserProfileApi.cs
@@ -0,0 +1,29 @@
+﻿using BlazorBoilerplate.CommonUI.Services.Contracts;
+using BlazorBoilerplate.Shared.Dto;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BlazorBoilerplate.Shared.Dto.Account;
+
+namespace BlazorBoilerplate.CommonUI.Services.Implementations
+{
+    public class UserProfileApi : IUserProfileApi
+    {
+        private readonly HttpClient _httpClient;
+
+        public UserProfileApi(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ApiResponseDto> Get()
+        {
+            return await _httpClient.GetJsonAsync<ApiResponseDto>("api/UserProfile/Get");
+        }
+
+        public async Task<ApiResponseDto> Upsert(UserProfileDto userProfile)
+        {
+            return await _httpClient.PostJsonAsync<ApiResponseDto>("api/UserProfile/Upsert", userProfile);
+        }
+    }
+}
---
+++ src/BlazorBoilerplate.Shared/Dto/Account/ConfirmEmailDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBoilerplate.Shared.Dto.Account
+{
+
+    public class ConfirmEmailDto
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+    }
+}
